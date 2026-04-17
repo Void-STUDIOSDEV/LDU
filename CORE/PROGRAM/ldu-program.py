c54eb8dd725e2e025c3fcf1640595bf7745702c3
Void-STUDIOSDEV <voiddevr@protonmail.com>
2026-04-17 01:53:58 -0400

    Refactor sleep timings and add help option
    Adjusted sleep durations and print statements for better user experience. Added help option for user guidance.

--- CORE/PROGRAM/ldu-program.py
+++ CORE/PROGRAM/ldu-program.py
@@ -26,7 +26,7 @@ def file_list():
 		files.sort(reverse=True)
 	
 		for s, p in files[:5]:
-			sleep(1); print(round(s/1024**3, 2), "GB", p) #adjust the sleep time, files will either appear faster or slower
+			sleep(1); print(round(s/1024**3, 2), "GB\n", p) #adjust the sleep time, files will either appear faster or slower
 #required modules, variables, and defs
 
 
@@ -34,7 +34,8 @@ sleep(0.5); print("\nLinux Disk Utilities, LDU")
 sleep(0.5); print("Version 01.00.00")
 sleep(0.5); print("COPYRIGHT VOID STUDIOS 2026")
 sleep(0.5); print("COVERED UNDER GNU General Public License 2.x\n")
-sleep(0.8); print("[E]XIT: Stops the program [does not auto-exit]")
+sleep(0.5); print("[H]ELP")
+sleep(0.8); print("[E]XIT: Stops the program")
 sleep(0.5); print("[D]ISK: Shows storage capabilities [Total, Free, Used]")
 sleep(0.5); print("[R]EAD: Lists largest files in HOME [~] directory")
 sleep(0.5); print("<>E: Adding 'E' to any selection does an auto-stop after it runs.")
@@ -44,28 +45,33 @@ sleep(0.5); print("<>E: Adding 'E' to any selection does an auto-stop after it r
 while True:
 	choice = input("SELECTION: ").upper()
 	if choice == "E":
-		sleep(0.5); print("Goodbye!")
-		sleep(0.4); print(".")
-		sleep(0.3); print("..")
-		sleep(0.2); print("...")
-		sleep(0.1); print("....")
-		sleep(0.0); print(".....")
+		sleep(0.1); print("Goodbye!")
+		sleep(0.2); print("..")
+		sleep(0.3); print("...")
+		sleep(0.4); print("....")
+		sleep(0.5); print(".....")
+		sleep(0.6); print("......")
 		break
-	#this part allows users to exit, allowing safe exiting
+	#this part allows users to exit, allowing safe exiting from the terminal after use
 	
 	if choice == "D":
 		disk_size()
 		break
-		
+
 	if choice == "R":
 		file_list()
-		break
-			
+
+
 	if choice == "DE":
 		disk_size()
+		break
 		
 	if choice == "RE":
 		file_list()
+		break
 		
-	if choice not in ["D", "R", "DE", "RE"]:
+	if choice == "H":
+		print("To switch the speed of how it lists things out, change the numbers in () for 'sleep' | For any issues, email: voiddevr@protonmail.com or N4llKer3nel@protonmail.com\n")
+		
+	if choice not in ["D", "R", "DE", "RE", "H"]:
 		print("INVALID SELECTION\n")
